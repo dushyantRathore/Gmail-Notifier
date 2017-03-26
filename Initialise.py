47140c3ac42d9c9ebdd4a78e953bb1fcc054fcf8
dushyantRathore <dushyant.bgs@gmail.com>
2017-03-26 14:25:34 +0530

    Setup Gmail Credentials

---
+++ Initialise.py
@@ -0,0 +1,37 @@
+import smtplib
+import imaplib
+import email
+import pickle
+
+# Initialisation Function
+
+
+def startup():
+
+    org_email = "@gmail.com"
+    smtp_server = "imap.gmail.com"
+    smtp_port = 993
+
+    # username = raw_input("Enter username : ")
+    # password = raw_input("Enter password : ")
+
+    username = "username"
+    password = "password"
+
+    mail = imaplib.IMAP4_SSL(smtp_server)
+
+    mail.login(username, password)
+    mail.select('inbox')
+
+    type, data = mail.search(None, 'ALL')
+    mail_ids = data[0]
+    id_list = mail_ids.split()
+
+    first_id = int(id_list[0])
+    last_id = int(id_list[-1])
+
+    fileobject = open('ID.txt', 'w')
+    pickle.dump(last_id, fileobject)
+
+
+startup()
